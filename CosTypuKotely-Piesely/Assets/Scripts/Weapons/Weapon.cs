3eaba6edfe0ceb52ced1fd14a4da76378f3fa082
yamaz0 <szulc.patryk1c@gmail.com>
2022-05-13 01:07:23 +0200

    Changed bullets as projectile to raycast

--- CosTypuKotely-Piesely/Assets/Scripts/Weapons/Weapon.cs
+++ CosTypuKotely-Piesely/Assets/Scripts/Weapons/Weapon.cs
@@ -6,10 +6,25 @@ public class Weapon : MonoBehaviour
     private Bullet bullet;
     [SerializeField]
     private SpriteRenderer spriteRenderer;
-
+    [SerializeField]
+    private TrailRenderer trailRenderer;
+    [SerializeField]
+    private ContactFilter2D filtr;
     public void Shoot(Vector2 direction)
     {
-        Bullet b = Instantiate(bullet, transform.position, Quaternion.identity);//TODO pooling
-        b.Init(direction);
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, direction.normalized, 15, filtr.layerMask);
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y);
+        Vector2 endpos = origin + direction.normalized * 15;
+
+        TrailRenderer createtTrail = Instantiate(trailRenderer, origin, Quaternion.identity);
+
+        createtTrail.AddPosition(origin);
+
+        if (raycastHit2D.collider != null && raycastHit2D.collider.tag == "Enemy")
+        {
+            endpos = raycastHit2D.collider.transform.position;
+            raycastHit2D.collider.GetComponent<Enemy>().TakeDamage(1);
+        }
+        createtTrail.transform.position = endpos;
     }
 }
