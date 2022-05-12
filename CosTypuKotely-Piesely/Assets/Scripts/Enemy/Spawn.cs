1156146ab8463887b9b05994026ff11cbb0b67d7
yamaz0 <szulc.patryk1c@gmail.com>
2022-05-12 11:44:06 +0200

    Better spawner

--- CosTypuKotely-Piesely/Assets/Scripts/Enemy/Spawn.cs
+++ CosTypuKotely-Piesely/Assets/Scripts/Enemy/Spawn.cs
@@ -7,17 +7,19 @@ public class Spawn : MonoBehaviour
     [SerializeField]
     private Enemy template;
     [SerializeField]
-    private Renderer spriteRenderer;
+    private float range = 12f;
+
+
     void Start()
     {
         InvokeRepeating("SpawnEnemy", 1, 1);
     }
+
     public void SpawnEnemy()
     {
-        float distance = Vector3.Distance(Player.Instance.gameObject.transform.position, transform.position);
-        // Debug.Log(distance);
-        if (spriteRenderer.isVisible == false && distance < 25)
-            Instantiate(template, transform);
+        Vector3 randomPoint = Random.insideUnitCircle.normalized * range;
+        Vector3 spawnPoint = Player.Instance.gameObject.transform.position + randomPoint;
+        Instantiate(template, spawnPoint, Quaternion.identity);
     }
 
 }
