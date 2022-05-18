0f76a8ac513253e43e4bfeefdb921b2e450f0b42
yamaz0 <szulc.patryk1c@gmail.com>
2022-05-18 21:59:03 +0200

    Added simple bullet upgrades

--- CosTypuKotely-Piesely/Assets/Scripts/Weapons/Weapon.cs
+++ CosTypuKotely-Piesely/Assets/Scripts/Weapons/Weapon.cs
@@ -1,15 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField]
-    private Bullet bullet;
+    private List<Bullet> bullets;
     [SerializeField]
     private SpriteRenderer spriteRenderer;
 
+    public int CurrentWeaponLevel { get; private set; } = 0;
+
+    public void UpgradeWeapon()
+    {
+        CurrentWeaponLevel++;
+    }
+
     public void Shoot(Vector2 direction)
     {
-        Bullet createdBullet = Instantiate(bullet, transform.position, Quaternion.identity);//todo pooling
+        Bullet createdBullet = Instantiate(bullets[CurrentWeaponLevel], transform.position, Quaternion.identity);//todo pooling
         createdBullet.Init(direction);
     }
 }
