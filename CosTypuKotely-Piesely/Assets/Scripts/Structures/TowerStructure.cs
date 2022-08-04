b42b37792d27281abd39fcfde09eb804491db1e6
yamaz0 <szulc.patryk1c@gmail.com>
2022-08-04 21:02:38 +0200

    Added LineRenderer to towers

--- CosTypuKotely-Piesely/Assets/Scripts/Structures/TowerStructure.cs
+++ CosTypuKotely-Piesely/Assets/Scripts/Structures/TowerStructure.cs
@@ -7,8 +7,11 @@ public class TowerStructure : StructureBase
     WaitForSeconds rotateWaitTime = new WaitForSeconds(.1f);
     [SerializeField]
     private LayerMask mask;
+    [SerializeField]
+    private LineRenderer lineRenderer;
 
     public float CacheTime { get; private set; }
+    public LineRenderer LineRenderer { get => lineRenderer; set => lineRenderer = value; }
 
     private void FixedUpdate()
     {
@@ -40,8 +43,41 @@ public class TowerStructure : StructureBase
         {
             //Shoot
             CacheTime = Time.unscaledTime;
+
+            Vector2 origin = new Vector2(transform.position.x, transform.position.y);
+            Vector2 endpos = new Vector2(Enemy.transform.position.x, Enemy.transform.position.y);
+
+            lineRenderer.SetPosition(0, origin);
+
             Enemy.TakeDamage(((TowerInfo)Info).Dmg);
+
+            lineRenderer.SetPosition(1, endpos);
+            StartCoroutine(FadeLineRenderer());
         }
+    }
+
+    public IEnumerator FadeLineRenderer()
+    {
+        Gradient lineRendererGradient = new Gradient();
+        float fadeSpeed = 3f;
+        float timeElapsed = 0f;
+        float alpha = 1f;
+
+        while (timeElapsed < fadeSpeed)
+        {
+            alpha = Mathf.Lerp(1f, 0f, timeElapsed / fadeSpeed);
+
+            lineRendererGradient.SetKeys
+            (
+                lineRenderer.colorGradient.colorKeys,
+                new GradientAlphaKey[] { new GradientAlphaKey(alpha, 1f) }
+            );
+            lineRenderer.colorGradient = lineRendererGradient;
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
     }
 
     private void SearchEnemy()
