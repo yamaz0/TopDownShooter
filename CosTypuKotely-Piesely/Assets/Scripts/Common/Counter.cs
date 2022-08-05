0347aa176b290fc5b421d7efb710d229721e99fe
yamaz0 <szulc.patryk1c@gmail.com>
2022-08-05 22:58:04 +0200

    Added counter class

---
+++ CosTypuKotely-Piesely/Assets/Scripts/Common/Counter.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Counter
+{
+    public int Value { get; private set; }
+    public int MaxValue { get; private set; }
+    public int MinValue { get; private set; }
+
+    public Counter(int value, int min = 0, int max = int.MaxValue)
+    {
+        Value = value;
+        MinValue = min;
+        MaxValue = max;
+    }
+
+    public void Set(int v)
+    {
+        Value = Mathf.Clamp(v, MinValue, MaxValue);
+    }
+
+    public void Increase()
+    {
+        Value++;
+        if (Value > MaxValue)
+            Value = MinValue;
+    }
+
+    public void Decrease()
+    {
+        Value--;
+        if (Value < MinValue)
+            Value = MaxValue;
+    }
+}
